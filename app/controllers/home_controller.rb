f9516131530acf979747ff6d4f2c22c17ee0d576
chrismayu <chrismayu@gmail.com>
2014-05-18 20:41:25 -0400

    If 3 post on first page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @users = User.all
     @message = Message.new
-    @recent_post = Post.first(3)
+    @recent_post = Post.where(:published => true).select{|post| post.display_date.to_date >= DateTime.now}.first(3)
   
   end
 end
