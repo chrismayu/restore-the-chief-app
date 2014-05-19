bd04fe34ebb1471fb671fb8a88141ead0faebc94
chrismayu <chrismayu@gmail.com>
2014-05-19 17:19:47 -0400

    Home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,5 @@ class HomeController < ApplicationController
     @users = User.all
     @message = Message.new
     @recent_post = Post.where(:published => true).select{|post| post.display_date.to_date >= DateTime.now}.first(3)
-  
   end
 end
